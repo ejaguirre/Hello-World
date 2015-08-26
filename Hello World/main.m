7d45504102abef3dce5b2a7d28ca7b20338987c7
Enrique Aguirre <enrique@Enrique-Aguirres-MacBook-Pro.local>
2015-08-26 15:07:39 -0700

    Initial Commit

---
+++ Hello World/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by Enrique Aguirre on 8/26/15.
+//  Copyright (c) 2015 Enrique Aguirre. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
